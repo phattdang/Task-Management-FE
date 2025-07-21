e1824f41c006a7ead714cfdc4b0095887a7f3da9
phatdang <phatdang19052004@gmail.com>
2025-07-21 19:22:56 +0700

    Kiểm thử gọi API từ back-end được deploy qua render

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [str, setStr] = useState("");
+
+  const sayHello = () => {
+    fetch("https://task-management-be-1-ey1a.onrender.com/sayHello")
+      .then((res) => res.json())
+      .then((res) => setStr(res));
+  };
+
+  console.log(str);
+
   return (
     <>
       <p>Hello</p>
+      <button onClick={sayHello}>Say hello</button>
     </>
   );
 }
